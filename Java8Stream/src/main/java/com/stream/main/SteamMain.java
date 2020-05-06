7d56b6f9b195936d9c61052e25c64e285e620009
leotar94 <tartaglialeonardo.94@gmail.com>
2020-05-06 15:10:57 +0200

    Second Commit

--- Java8Stream/src/main/java/com/stream/main/SteamMain.java
+++ Java8Stream/src/main/java/com/stream/main/SteamMain.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.Hashtable;
 
 import com.stream.Album;
 
@@ -26,70 +27,105 @@ public class SteamMain {
 			if(album.getAnno()<2000)
 				System.out.println(album.getAutore());
 		}
-		albums.stream().filter(album ->album.getAnno()<2000).map(Album::getAutore).forEach(System.out::println);
+		albums.stream().filter(album ->album.getAnno()<2000)
+				.map(Album::getAutore)
+				.forEach(System.out::println);
 		/*
-		 * Ma cos’è allora uno stream? Nessuna parentela con 
-		 * InputStream o OutputStream. Uno stream è un’interfaccia
-		 *  che restituisce un flusso di dati finito o infinito 
-		 *  su cui è possibile fare operazioni di filtro,
+		 * Ma cosï¿½ï¿½ allora uno stream? Nessuna parentela con
+		 * InputStream o OutputStream. Uno stream ï¿½ unï¿½interfaccia
+		 *  che restituisce un flusso di dati finito o infinito
+		 *  su cui ï¿½ possibile fare operazioni di filtro,
 		 *  mappa e riduzione (leggiamo aggregazione per il momento).
-		 *  Può operare in modo sequenziale (che è il default) oppure
-		 *  a richiesta in modo parallelo. Possiamo vedere lo stream 
-		 *  come una pipeline di operazioni su dati che, come ogni 
-		 *  tubatura che si rispetti, ha una sorgente, una serie 
+		 *  Puï¿½ operare in modo sequenziale (che ï¿½ il default) oppure
+		 *  a richiesta in modo parallelo. Possiamo vedere lo stream
+		 *  come una pipeline di operazioni su dati che, come ogni
+		 *  tubatura che si rispetti, ha una sorgente, una serie
 		 *  di pezzi intermedi e una destinazione.
 		 */
-		
+
 		//OPERAZIONI INTERMEDIE
 		Stream<Album> stream1=albums.stream();
 		Stream<Album> stream2= stream1.filter(album->album.getAnno()<2000);
 		/*
 		 * Come conferma la documentazione, questa operazione (filter)
-		 * restituisce uno stream che soddisfa un Predicate 
-		 * passato come parametro. Un Predicate (predicato in italiano) 
-		 * è una interfaccia funzionale (come poteva non esserlo, 
-		 * abbiamo passato una lambda ) che ha un unico compito: 
-		 * quello di rispondere vero o falso nel metodo test. 
-		 * Gli oggetti che soddisfano il test andranno avanti 
+		 * restituisce uno stream che soddisfa un Predicate
+		 * passato come parametro. Un Predicate (predicato in italiano)
+		 * ï¿½ una interfaccia funzionale (come poteva non esserlo,
+		 * abbiamo passato una lambda ) che ha un unico compito:
+		 * quello di rispondere vero o falso nel metodo test.
+		 * Gli oggetti che soddisfano il test andranno avanti
 		 * nella pipeline, gli altri saranno scartati.
 		 */
 		Stream<String> stream3=stream2.map(Album::getAutore);
 		/*
-		 * Anche map torna uno stream, però, a differenza di filter, 
-		 * ci permette di cambiare il tipo di oggetti nello stream 
-		 * passando una lambda che sia compatibile con l’interfaccia 
-		 * funzionale Function. Questa interfaccia richiede di 
-		 * implementare il metodo apply che prende un oggetto di 
-		 * tipo T, quello delle stream corrente e torna un oggetto 
-		 * di tipo R. Nell’esempio di sopra, ad uno stream di Album 
+		 * Anche map torna uno stream, perï¿½, a differenza di filter,
+		 * ci permette di cambiare il tipo di oggetti nello stream
+		 * passando una lambda che sia compatibile con lï¿½interfaccia
+		 * funzionale Function. Questa interfaccia richiede di
+		 * implementare il metodo apply che prende un oggetto di
+		 * tipo T, quello delle stream corrente e torna un oggetto
+		 * di tipo R. Nellï¿½esempio di sopra, ad uno stream di Album
 		 * passiamo una lambda che dato un album ritorna il suo titolo.
 		 */
 		stream3.forEach(System.out::println);
-		
+
 		//OPERAZIONE TERMINALE
 		List<String> autori= albums.stream().filter(album-> album.getAnno()<2000)
 				.map(Album::getAutore).collect(Collectors.toList());
-		double avgCanzoni = albums.stream().mapToInt(album->album.getCanzoni().size())
+		double avgCanzoni = albums.stream()
+				.mapToInt(album->album.getCanzoni().size())
 				.average().getAsDouble();
 		System.out.println(avgCanzoni);
 		/*
-		 * Nel primo esempio, al posto di applicare un forEach, 
-		 * tramite il metodo collect riversiamo il risultato dello 
-		 * stream in una lista. Nel secondo, torniamo per ogni 
-		 * album il numero di canzoni e ne calcoliamo la media 
-		 * (come double). Notiamo una cosa: il metodo average non è 
-		 * disponibile per qualsiasi stream, ma solo per quelli numerici, 
-		 * per cui non possiamo usare semplicemente map ma dobbiamo convertire 
+		 * Nel primo esempio, al posto di applicare un forEach,
+		 * tramite il metodo collect riversiamo il risultato dello
+		 * stream in una lista. Nel secondo, torniamo per ogni
+		 * album il numero di canzoni e ne calcoliamo la media
+		 * (come double). Notiamo una cosa: il metodo average non ï¿½
+		 * disponibile per qualsiasi stream, ma solo per quelli numerici,
+		 * per cui non possiamo usare semplicemente map ma dobbiamo convertire
 		 * lo stream in un IntStream tramite mapToInt.
 		 */
-		
+		//in JAVA7
+		int limit = 10;
+		List<String> songs = new ArrayList<>();
+		for (Album album : albums) {
+			if (album.getYear() < 2000) {
+				songs.addAll(album.getSongs());
+			}
+		}
+		Collections.sort(songs);
+		for (int i = 0; i < limit; i++) {
+			System.out.println(songs.get(i));
+		}
+
+		//in JAVA8
 		albums.stream()
 	    .filter(album -> album.getAnno() < 2000)
 	    .flatMap(album -> album.getCanzoni().stream())
 	    .sorted()
 	    .limit(5)
 	    .forEach(System.out::println);
-		
+		//se vogliamo aggiungere filtri
+		//JAVA7
+		int limit = 10;
+		Set<String> songs = new TreeSet<>();
+		for (Album album : albums) {
+			if (album.getYear() < 2000) {
+				for (String song : album.getSongs()) {
+					if (song.startsWith("D"))
+						songs.add(song);
+				}
+			}
+		}
+		int temp = 0;
+		for (String s : songs) {
+			System.out.println(s);
+			temp++;
+			if (temp == limit)
+				break;
+		}
+		//JAVA 8
 		albums.stream()
 	    .filter(album -> album.getAnno() < 2000)
 	    .flatMap(album -> album.getCanzoni().stream().filter(s -> s.startsWith("A"))) //case sensitive
@@ -97,8 +133,22 @@ public class SteamMain {
 	    .sorted()
 	    .limit(10)
 	    .forEach(System.out::println);
-		
-		
+
+		//come fare in un ambito di sviluppo multithread?, non Ã¨
+		//un problema... basta utilizzare semplicemente parallelStream.
+		albums.parallelStream()
+				.flatMap(album->album.getCanzoni().stream())
+				.count();
+		//la notazione rimane la stessa, cambia solo parallel stream
+		/*
+		NOTA BENE, non bisogna usare il foreach in parallelo, potrebbe
+		avviare un'eccezione o potebbe estituie risultati diversi
+		dovuti all'esecuzione multithread
+		soluzione? Buttiamo lo stream in una lista o map...
+		 */
+
+
+
 	}
 
 }
